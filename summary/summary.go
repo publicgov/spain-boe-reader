8c5e33c8fa6a1e6ac3d2912d507f892dec2583ce
newhouse <nhitbh@gmail.com>
2017-09-26 19:46:43 +0200

    Added new structs to store the pdf summary info

--- summary/summary.go
+++ summary/summary.go
@@ -20,5 +20,19 @@ type Metadata struct {
 // Diary contains the information of the dispositions that conform
 // each of the bulletins on a certain date
 type Diary struct {
-	BulletinNumber string `xml:"nbo,attr"`
+	BulletinNumber string     `xml:"nbo,attr"`
+	Summary        PdfSummary `xml:"sumario_nbo"`
+}
+
+// PdfSummary contains information of the pdf summary associated to the bulletin
+type PdfSummary struct {
+	ID   string  `xml:"id,attr"`
+	Info PdfInfo `xml:"urlPdf"`
+}
+
+// PdfInfo contains file info, like number of pages and size
+type PdfInfo struct {
+	SizeBytes  string `xml:"szBytes,attr"`
+	SizeKBytes string `xml:"szKBytes,attr"`
+	Pages      string `xml:"numPages,attr"`
 }
