75f6178f9cb7c4b754768eca24466fbd3151426d
newhouse <nhitbh@gmail.com>
2017-09-26 22:38:48 +0200

    Fixed error with the xml encoding. Now the reader is converted to utf-8 before parsing

--- main.go
+++ main.go
@@ -1,33 +1,5 @@
 package main
 
-import (
-	"encoding/xml"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-
-	"github.com/publicgov/spain-boe-reader/summary"
-)
-
 func main() {
-	summary := readXML()
-	bulletinType := summary.Meta.BulletinType
-	fmt.Println(bulletinType)
-}
 
-func readXML() summary.BoeSummary {
-	file := ""
-	xmlFile, err := os.Open(file)
-	if err != nil {
-		log.Fatalln("Error opening file:", err)
-	}
-	defer xmlFile.Close()
-	content, err := ioutil.ReadAll(xmlFile)
-	if err != nil {
-		log.Fatalln("Error reading content from", err)
-	}
-	var summary summary.BoeSummary
-	xml.Unmarshal(content, &summary)
-	return summary
 }
--- summary/summary_test.go
+++ summary/summary_test.go
@@ -2,17 +2,30 @@ package summary
 
 import (
 	"encoding/xml"
-	"io/ioutil"
 	"log"
 	"os"
 	"testing"
+
+	"golang.org/x/net/html/charset"
 )
 
-func TestBoeSummary(t *testing.T) {
+func TestMetadata(t *testing.T) {
 	summary := readXML()
 	bulletinType := summary.Meta.BulletinType
+	lastDate := summary.Meta.LastPublicationDate
+	nextDate := summary.Meta.NextPublicationDate
+	pubDate := summary.Meta.PublicationDate
 	if bulletinType != "BOE" {
 		t.Error("Expected bulletin type BOE, got", bulletinType)
+	}
+	if lastDate != "17/10/2014" {
+		t.Error("Expected last data 18/10/2014, got", lastDate)
+	}
+	if nextDate != "20/10/2014" {
+		t.Error("Expected last data 20/10/2014, got", nextDate)
+	}
+	if pubDate != "18/10/2014" {
+		t.Error("Expected last data 18/10/2014, got", pubDate)
 	}
 }
 
@@ -22,11 +35,11 @@ func readXML() BoeSummary {
 		log.Fatalln("Error opening file:", err)
 	}
 	defer xmlFile.Close()
-	content, err := ioutil.ReadAll(xmlFile)
-	if err != nil {
-		log.Fatalln("Error reading content from", err)
-	}
+
 	var summary BoeSummary
-	xml.Unmarshal(content, &summary)
+	decoder := xml.NewDecoder(xmlFile)
+	decoder.CharsetReader = charset.NewReaderLabel
+	err = decoder.Decode(&summary)
+
 	return summary
 }
